590a34ec94290c939eeb835e3013addafe85bc09
OvermindDL1 <overminddl1@gmail.com>
2013-08-02 22:37:04 -0400

    Updated to rebar OTP structure.

--- startdev.sh
+++ startdev.sh
@@ -2,4 +2,4 @@
 clear
 rebar compile
 #erl -mnesia dir '"./mnesia"' -pa apps/*/ebin deps/*/ebin -eval 'sync:go(), application:start(eirc), application:start(overbot).'
-erl -mnesia dir '"./mnesia"' -pa ./ebin apps/*/ebin deps/*/ebin -eval 'application:start(msw).'
+erl -mnesia dir '"./mnesia"' -pa apps/*/ebin deps/*/ebin -eval 'application:start(msw).'
