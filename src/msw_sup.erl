91e99baedfffbd0a0321608701998ed552484adb
OvermindDL1 <overminddl1@gmail.com>
2013-08-02 22:04:38 -0400

    Basic node structure, setup the webserver part.

--- src/msw_sup.erl
+++ src/msw_sup.erl
@@ -11,6 +11,8 @@
 %% Helper macro for declaring children of supervisor
 -define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
 
+-define(APP, msw_app).
+
 %% ===================================================================
 %% API functions
 %% ===================================================================
@@ -23,5 +25,20 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-	{ok, { {one_for_one, 5, 10}, []} }.
+	{ok, { {one_for_one, 5, 10}, [
+			%% Name, NbAcceptors, TransOpts, ProtoOpts
+		{webserver, {cowboy, start_http, [webserver, 100, [{port, 8000}], [{env, [{dispatch, dispatch_rules()}]}]]}}
+		]} }.
 
+
+
+dispatch_rules() ->
+	cowboy_router:compile(
+	%% {URIHost, [{URIPath, Handler, Opts}]}
+		[{'_', [
+			{["/static/[...]"], cowboy_static,
+				[{directory, {priv_dir, ?APP, [<<"static">>]}},
+					{mimetypes, {fun mimetypes:path_to_mimes/2, default}}]},
+			{["/ws/[...]"], n2o_websocket, []},
+			{'_', n2o_cowboy, []}
+			]}]).
