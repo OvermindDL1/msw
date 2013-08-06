7de5a5393a6c364b6afdd59f6cd1908dcd654cb0
OvermindDL1 <overminddl1@gmail.com>
2013-08-06 00:13:28 -0400

    Added a bcrypt based secure password/matching library.

--- apps/msw/src/msw_app.erl
+++ apps/msw/src/msw_app.erl
@@ -9,7 +9,7 @@
 	stop/1
 	]).
 
--define(APPS, [sasl, lager, sync, exec, crypto, ranch, cowboy, asn1, public_key, ssl, n2o, msw]).
+-define(APPS, [sasl, lager, sync, exec, crypto, ranch, cowboy, asn1, public_key, ssl, n2o, bcrypt, erlpass, msw]).
 
 %% ===================================================================
 %% Application callbacks
