ce99f5b4af1a4cff4aa36f65c957aa06825d8ed5
OvermindDL1 <overminddl1@gmail.com>
2013-08-06 00:19:08 -0400

    Going to use leveldb instead of mnesia, faster and without memory limits, but is not scalable across nodes, however that can be fixed in a later version if really wanted, switch to riak or something.

--- apps/msw/src/msw_app.erl
+++ apps/msw/src/msw_app.erl
@@ -9,7 +9,7 @@
 	stop/1
 	]).
 
--define(APPS, [sasl, lager, sync, exec, crypto, ranch, cowboy, asn1, public_key, ssl, n2o, bcrypt, erlpass, msw]).
+-define(APPS, [sasl, lager, sync, exec, crypto, ranch, cowboy, asn1, public_key, ssl, n2o, bcrypt, erlpass, eleveldb, msw]).
 
 %% ===================================================================
 %% Application callbacks
